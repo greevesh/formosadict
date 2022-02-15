6a0d16a04c039d424160d0a5f5ba6b1ee95dc044
Harri <hgreeves@protonmail.com>
2022-02-15 11:35:20 +0800

    Definition arrays have been converted into strings so Instantsearch can parse them.

--- dict-data-formatter.js
+++ dict-data-formatter.js
@@ -1,14 +1,17 @@
 const translations = require('./cc-cedict.json')
 const fs = require('fs')
 
-translations.filter(key => delete key.referencedTraditional)
-translations.filter(key => delete key.referencedSimplified)
-translations.filter(key => delete key.definitionsDiacritic)
+translations.filter(translation => delete translation.referencedTraditional)
+translations.filter(translation => delete translation.referencedSimplified)
+translations.filter(translation => delete translation.definitionsDiacritic)
+
+// allows instantsearch to parse the definitions
+translations.forEach(translation => translation["definitions"] = translation["definitions"].join(" | "))
 
 let id = 1
 
-// adds id key so MeiliSearch can index it
-translations.forEach(key => key.id = id++)
+// adds id translation so MeiliSearch can index it
+translations.forEach(translation => translation.id = id++)
 
 fs.appendFile('translations.json', JSON.stringify(translations), (err) => {
     err ? console.log(err) : console.log('Saved!')
