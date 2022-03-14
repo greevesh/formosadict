f82b38752512dcd797a9f4de9a6ca4e5ba9f2e12
Harri <hgreeves@protonmail.com>
2022-03-15 00:45:53 +0800

    China flag emoji can now be added to words only used in China (found in the 'Simplified' column).

--- dict-data-formatter.js
+++ dict-data-formatter.js
@@ -1,18 +1,34 @@
-// const translations = require('./cc-cedict.json')
-// const fs = require('fs')
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+const translations = require('./cc-cedict.json')
+const fs = require('fs')
 
-// translations.filter(translation => delete translation.referencedTraditional)
-// translations.filter(translation => delete translation.referencedSimplified)
-// translations.filter(translation => delete translation.definitionsDiacritic)
+translations.filter(translation => delete translation.referencedTraditional)
+translations.filter(translation => delete translation.referencedSimplified)
+translations.filter(translation => delete translation.definitionsDiacritic)
 
-// // allows instantsearch to parse the definitions
-// translations.forEach(translation => translation["definitions"] = translation["definitions"].join(" | "))
+const china = [
+    '意大利',
+    '柬埔寨',
+    '老挝',
+    '朝鲜',
+    '马尔代夫',
+    '新西兰',
+    '悉尼',
+    '戛纳',
+    '佛罗伦萨'
+]
 
-// let id = 1
+translations.forEach(translation => china.includes(translation.simplified) ? translation.simplified = translation.simplified.concat(" 🇨🇳") : null)
 
-// // adds id translation so MeiliSearch can index it
-// translations.forEach(translation => translation.id = id++)
+// allows instantsearch to parse the definitions
+translations.forEach(translation => translation["definitions"] = translation["definitions"].join(" | "))
 
-// fs.appendFile('translations.json', JSON.stringify(translations), (err) => {
-//     err ? console.log(err) : console.log('Saved!')
-// })
+let id = 1
+
+// adds id translation so MeiliSearch can index it
+translations.forEach(translation => translation.id = id++)
+
+fs.appendFile('translations.json', JSON.stringify(translations), err => {
+    err ? console.log(err) : console.log('Saved!')
+})
