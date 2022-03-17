c05786a89a694cb284ab70405a6024c7b8072d53
Harri <hgreeves@protonmail.com>
2022-03-18 01:27:56 +0800

    Attempting to replace two submodules with one.

---
+++ flashcards/src/views/SignUpView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <form @submit.prevent="signIn">
+      <h2>Sign Up</h2>
+      <input type="email" v-model="email" placeholder="Type your email"><br>
+      <input type="password" v-model="password" placeholder="Type your password"><br>
+      <input type="password" v-model="password" placeholder="Type your password"><br>
+      <RouterLink to="/sign-in"><button>Sign In</button></RouterLink>
+        &nbsp;
+      <RouterLink to="/sign-up"><button>Sign Up</button></RouterLink>
+    </form>
+  </div>
+  
+
+</template>
+
+<style>
+
+</style>
