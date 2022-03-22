144222a464a0b8741cdbf1b634401c8bec40268c
Harri <hgreeves@protonmail.com>
2022-03-22 22:41:56 +0800

    Basic markup for flashcard deck has been added.

--- flashcards/src/App.vue
+++ flashcards/src/App.vue
@@ -6,8 +6,27 @@ import { RouterView } from "vue-router";
   <header>
   
     <div class="wrapper">
-
+      <div class="flashcard-container">
+        <div class="flashcards">
+          <div class="flashcards__instructions">
+            <p>Click card to see the term</p>
+          </div>
+          <div class="flashcards__translation">
+            <p>隱形</p>
+          </div>
+          <div class="flashcards__cross-check">
+            <span class="x">X</span>
+            <span class="check"></span>
+          </div>
+          <div class="flashcards__nav-score">
+            <p>Previous</p>
+            <p>0/1</p>
+            <p>Next</p>
+          </div>
+        </div>
+      </div>
     </div>
+    
   </header>
 
   <RouterView />
