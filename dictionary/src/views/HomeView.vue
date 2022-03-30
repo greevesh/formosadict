682ab50fc69b130f4f4dc9cf4898b249d516f3b6
Harri <hgreeves@protonmail.com>
2022-03-30 11:06:18 +0800

    Imported all main components into home view.

--- dictionary/src/views/HomeView.vue
+++ dictionary/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup>
+  import BaseNavBar from '../components/BaseNavbar.vue'
   import TheSuggestTranslationBar from '../components/TheSuggestTranslationBar.vue'
+  import TheHeading from '../components/TheHeading.vue'
+  import TheSubheading from '../components/TheSubheading.vue'
+  import TheSearchBar from '../components/TheSearchBar.vue'
+  import SearchLabels from '../components/SearchLabels.vue'
+  import SearchResults from '../components/SearchResults.vue'
+  import TheWordOfTheDay from '../components/TheWordOfTheDay.vue'
+  import SuggestTranslationForm from '../components/SuggestTranslationForm.vue'
+  import TheFooter from '../components/TheFooter.vue'
 </script>
 
 <template>
