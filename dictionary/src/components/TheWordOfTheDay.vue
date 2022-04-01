f12b122cb179d121d49667cd9f07d2bbc9408d0d
Harri <hgreeves@protonmail.com>
2022-04-02 01:39:04 +0800

    WOTD markup has been added.

---
+++ dictionary/src/components/TheWordOfTheDay.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="wotd" class="wotd">
+            <section class="wotd__container">
+              <h2>Word of the Day</h2>
+              <div class="wotd__translation">
+                <p style="font-weight: bold; font-size: 25px;" id="chinese" class="wotd__text">隱形</p>
+                <p id="pinyin" class="wotd__text">yǐn xíng</p>
+                <p id="definition" class="wotd__text">invisible</p>
+                <!-- <a href="https://twitter.com/intent/tweet?text=FormosaDict's%20word%20of%20the%20day%20is%20'wotd'" class="twitter-share-button" data-show-count="false" data-size="large"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
+              </div>
+            </section>
+          </div>
+</template>
+
+<style>
+    /* .wotd {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+  
+    &__container {
+      font-size: $h2-font-size;
+      padding-top: 5px;
+      padding-bottom: 5px;
+      height: 30vh;
+      width: 45vw;
+      border-radius: 250px;
+    }
+    
+    &__translation {
+      text-align: center;
+    
+      &__text:nth-child(1) {
+        font-size: 25px;
+        font-weight: bold;
+      }
+    } */
+</style>
