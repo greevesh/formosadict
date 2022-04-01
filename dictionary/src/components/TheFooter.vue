d2a44b6b1cda08539edf485adf807541aef5d971
Harri <hgreeves@protonmail.com>
2022-04-02 01:51:50 +0800

    Footer component has been created.

---
+++ dictionary/src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+          <footer class="footer">
+            <p>Copyright © 2022 FormosaDict</p>
+          </footer>
+        </section>
+</template>
+
+<style>
+    .footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 8vh;
+  background-color: $secondary-color;
+  margin-top: 100px;
+  /* color: #fff; */
+}
+</style>
