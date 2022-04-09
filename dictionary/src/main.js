cf78d6dc8a9d75bdc0da6b867cbdbfff01612bee
Harri <hgreeves@protonmail.com>
2022-04-09 20:06:53 +0800

    Vuelidate installed and imported.

--- dictionary/src/main.js
+++ dictionary/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuelidate from 'vuelidate';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(Vuelidate)
 
 app.mount("#app");
