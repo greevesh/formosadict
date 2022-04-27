5a359f2ce382b3539dadef2833024e989119fdb2
Harri <hgreeves@protonmail.com>
2022-04-27 13:51:12 +0800

    Secondary content is now responsive.

--- dictionary/src/components/SecondaryContent.vue
+++ dictionary/src/components/SecondaryContent.vue
@@ -1,19 +1,36 @@
 <script setup>
-    import TheWordOfTheDay from './TheWordOfTheDay.vue'
-    import SuggestTranslationForm from './SuggestTranslationForm.vue'
+import TheWordOfTheDay from "./TheWordOfTheDay.vue";
+import SuggestTranslationForm from "./SuggestTranslationForm.vue";
 </script>
 
 <template>
-    <div class="secondary-content">
-        <TheWordOfTheDay />
-        <SuggestTranslationForm />
-    </div>
+  <div class="secondary-content">
+    <TheWordOfTheDay />
+    <SuggestTranslationForm />
+  </div>
 </template>
 
 <style>
-    .secondary-content {
-        display: flex;
-        align-items: center;
-        height: 60vh;
-    }
+.secondary-content {
+  display: flex;
+  align-items: center;
+  height: 60vh;
+}
+
+@media (max-width: 720px) {
+  .secondary-content {
+    display: block;
+    height: 110vh;
+  }
+
+  #suggest-form {
+    margin-top: 80px;
+  }
+}
+
+@media (max-width: 375px) {
+  .secondary-content {
+    height: 120vh;
+  }
+}
 </style>
