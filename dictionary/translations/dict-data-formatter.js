ef825aeaa2afb72207a097bb31b74fd4d7c1210f
Harri <hgreeves@protonmail.com>
2022-06-05 14:19:30 +0800

    Some translations now have a corresponding emoji and country of origin initial next to it.

--- dictionary/translations/dict-data-formatter.js
+++ dictionary/translations/dict-data-formatter.js
@@ -1,58 +1,39 @@
-import { createRequire } from "module"
-const require = createRequire(import.meta.url)
-const translations = require('./cc-cedict.json')
-const fs = require('fs')
+import { createRequire } from "module";
+import taiwaneseMandarinWords from "./taiwanese-mandarin-words.js";
+import chineseMandarinWords from "./chinese-mandarin-words.js";
+const require = createRequire(import.meta.url);
+const translations = require("./cc-cedict.json");
+const fs = require("fs");
 
-translations.filter(translation => delete translation.referencedTraditional)
-translations.filter(translation => delete translation.referencedSimplified)
-translations.filter(translation => delete translation.definitionsDiacritic)
+translations.filter((translation) => delete translation.referencedTraditional);
+translations.filter((translation) => delete translation.referencedSimplified);
+translations.filter((translation) => delete translation.definitionsDiacritic);
 
-const china = [
-    '意大利',
-    '柬埔寨',
-    '老挝',
-    '朝鲜',
-    '马尔代夫',
-    '新西兰',
-    '悉尼',
-    '戛纳',
-    '佛罗伦萨',
-    '橙子',
-    '菠萝',
-    '猕猴桃',
-    '番石榴',
-    '章鱼',
-    '吞拿鱼/金枪鱼',
-    '三文鱼',
-    '御田',
-    '酸奶',
-    '凉粉',
-    '巨无霸',
-    '赛百味',
-    '方便面',
-    '蒸蒸糕',
-    '花生',
-    '河粉',
-    '粉丝',
-    '西兰花',
-    '冰棒',
-    '薯片',
-    '空心菜',
-    '巧克力',
-    '圣代',
-    '现场做的蛋卷冰激淋'
-]
+translations.forEach((translation) =>
+  taiwaneseMandarinWords.includes(translation.traditional)
+    ? (translation.traditional = translation.traditional.concat(" 🧋 🇹🇼"))
+    : null,
+);
 
-translations.forEach(translation => china.includes(translation.simplified) ? translation.simplified = translation.simplified.concat(" 🇨🇳") : null)
+translations.forEach((translation) =>
+  chineseMandarinWords.includes(translation.simplified)
+    ? (translation.simplified = translation.simplified.concat(" 🐼 🇨🇳"))
+    : null,
+);
+
+console.log(taiwaneseMandarinWords);
 
 // allows instantsearch to parse the definitions
-translations.forEach(translation => translation["definitions"] = translation["definitions"].join(" | "))
+translations.forEach(
+  (translation) =>
+    (translation["definitions"] = translation["definitions"].join(" | ")),
+);
 
-let id = 1
+let id = 1;
 
 // adds id translation so MeiliSearch can index it
-translations.forEach(translation => translation.id = id++)
+translations.forEach((translation) => (translation.id = id++));
 
-fs.appendFile('translations.json', JSON.stringify(translations), err => {
-    err ? console.log(err) : console.log('Saved!')
-})
+fs.appendFile("translations.json", JSON.stringify(translations), (err) => {
+  err ? console.log(err) : console.log("Saved!");
+});
