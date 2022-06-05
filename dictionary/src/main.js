e5b9b22e817d3095a60dac66a05b40bcc98abd52
Harri <hgreeves@protonmail.com>
2022-06-05 14:11:55 +0800

    Vuelidate has been removed.

--- dictionary/src/main.js
+++ dictionary/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Vuelidate from 'vuelidate';
+import { useVuelidate } from "@vuelidate/core";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(Vuelidate)
+app.use(useVuelidate);
 
 app.mount("#app");
