32bb993080b06f1218e61b167c3c320f5ff98e6f
Harri <hgreeves@protonmail.com>
2022-06-05 14:25:23 +0800

    Removed Vuelidate reference.

--- dictionary/src/main.js
+++ dictionary/src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { useVuelidate } from "@vuelidate/core";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(useVuelidate);
 
 app.mount("#app");
