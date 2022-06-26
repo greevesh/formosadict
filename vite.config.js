a09ec6ac02267a8b807b3e4e37976329bed027c9
Harri <hgreeves@protonmail.com>
2022-06-27 00:47:38 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
   plugins: [vue()],
   resolve: {
     alias: {
