113651e21ad1931697be260d3c10bcf95002f936
Harri <hgreeves@protonmail.com>
2022-06-28 12:15:03 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+// import router from "./router";
 
 const app = createApp(App);
 
-app.use(router);
+// app.use(router);
+console.log("hi");
 
 app.mount("#app");
